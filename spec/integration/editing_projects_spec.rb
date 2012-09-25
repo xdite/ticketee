353d87eab3defe627a1502bd1c61df32d2b3eeaf
xdite <xdite@rocodev.com>
2012-09-25 16:10:47 +0800

    refactor spec

--- spec/integration/editing_projects_spec.rb
+++ spec/integration/editing_projects_spec.rb
@@ -1,11 +1,15 @@
 require 'spec_helper'
 
 feature "Editing Projects" do
-  scenario "Updating a project" do
-    project = FactoryGirl.create(:project, :name => "TextMate 2")
+
+  before do
+    FactoryGirl.create(:project, :name => "TextMate 2")
     visit "/"
     click_link "TextMate 2"
     click_link "Edit Project"
+  end
+
+  scenario "Updating a project" do
     fill_in "Name", :with => "TextMate 2 beta"
     click_button "Update Project"
     page.should have_content("Project has been updated.")
