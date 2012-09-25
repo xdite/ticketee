33ecdf381376ab70d6e761a1f916a6888b6af1c0
xdite <xdite@rocodev.com>
2012-09-25 18:06:10 +0800

    Add admin namespaced users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Ticketee::Application.routes.draw do
 
+  get "users/index"
+
   devise_for :users
   
   resources :projects do 
     resources :tickets
   end
+
+  namespace :admin do
+    resources :users
+  end
+
+
   root :to => "projects#index"
 end
